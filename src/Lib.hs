0fd1f94df0c384d94307cb832d260ff252a5c581
Sandy Maguire <sandy@sandymaguire.me>
2018-12-24 14:06:37 -0500

    make it feel good

--- src/Lib.hs
+++ src/Lib.hs
@@ -28,6 +28,53 @@ data Chord a
   deriving (Eq, Ord, Show, Functor)
 
 
+showPitch :: PitchClass -> String
+showPitch Ass = "A##"
+showPitch As  = "A#"
+showPitch A   = "A"
+showPitch Af  = "Ab"
+showPitch Aff = "Abb"
+showPitch Bss = "B##"
+showPitch Bs  = "B#"
+showPitch B   = "B"
+showPitch Bf  = "Bb"
+showPitch Bff = "Bbb"
+showPitch Css = "C##"
+showPitch Cs  = "C#"
+showPitch C   = "C"
+showPitch Cf  = "Cb"
+showPitch Cff = "Cbb"
+showPitch Dss = "D##"
+showPitch Ds  = "D#"
+showPitch D   = "D"
+showPitch Df  = "Db"
+showPitch Dff = "Dbb"
+showPitch Ess = "E##"
+showPitch Es  = "E#"
+showPitch E   = "E"
+showPitch Ef  = "Eb"
+showPitch Eff = "Ebb"
+showPitch Fss = "F##"
+showPitch Fs  = "F#"
+showPitch F   = "F"
+showPitch Ff  = "Fb"
+showPitch Fff = "Fbb"
+showPitch Gss = "G##"
+showPitch Gs  = "G#"
+showPitch G   = "G"
+showPitch Gf  = "Gb"
+showPitch Gff = "Gbb"
+
+
+showChord :: Chord PitchClass -> String
+showChord (Maj  r)   = showPitch r
+showChord (Maj7 r)   = showPitch r ++ "M7"
+showChord (Dom7 r)   = showPitch r ++ "7"
+showChord (Min  r)   = showPitch r ++ "m"
+showChord (Min7 r)   = showPitch r ++ "m7"
+showChord (Over c b) = showChord c ++ "/" ++ showPitch b
+
+
 rotate5 :: PitchClass -> PitchClass
 rotate5 pc = fst $ trans 7 (pc, 4)
 
@@ -46,26 +93,35 @@ matches (Over c b) ps
        in fst bp == b && matches c ps'
 
 
-matchSemitones :: PitchClass -> [Int] -> Set Pitch -> Bool
-matchSemitones r ts ps = (== Set.map fst ps)
-                       . Set.fromList
-                       $ fmap (fst . flip trans (r, 4)) ts
+matchSemitones
+    :: PitchClass
+    -> [Int]
+    -> Set Pitch
+    -> Bool
+matchSemitones r ts ps
+  = (== Set.map fst ps)
+  . Set.fromList
+  $ fmap (fst . flip trans (r, 4)) ts
 
 
 main :: IO ()
 main = flip evalStateT (Min C)
-     . S.print
+     . S.mapM_ (liftIO . putStrLn . showChord)
+     . S.map (fmap canonicalize)
      . S.mapMaybeM onChord
      . keysDown
      $ midiStream 20
 
 
-onChord :: MonadState (Chord PitchClass) m => Set Pitch -> m (Maybe (Chord PitchClass))
+onChord
+    :: MonadState (Chord PitchClass) m
+    => Set Pitch
+    -> m (Maybe (Chord PitchClass))
 onChord ps = do
   c <- get
   case matches c ps of
     True  -> do
-      let c' = fmap (canonicalize . rotate5) c
+      let c' = fmap rotate5 c
       put c'
       pure $ Just c'
     False -> pure Nothing
