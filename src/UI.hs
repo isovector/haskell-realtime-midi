1b8a01713dac7e17fb8bca6cba73a37b19631e70
Sandy Maguire <sandy@sandymaguire.me>
2018-12-30 22:45:05 -0500

    noice one bruh

--- src/UI.hs
+++ src/UI.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE DeriveGeneric    #-}
 {-# LANGUAGE TypeApplications #-}
 
 module UI where
 
+import Control.Concurrent (forkIO)
 import           Brick.AttrMap
 import           Brick.BChan
 import           Brick.Main
@@ -12,9 +15,20 @@ import           Brick.Widgets.Border.Style
 import           Brick.Widgets.Center
 import           Brick.Widgets.Core
 import           Brick.Widgets.ProgressBar
+import           Control.Comonad.Cofree (Cofree (..), unwrap, unfold)
+import           Control.Lens hiding ((:<))
+import           Control.Monad (void)
+import           Data.Coerce (coerce)
+import           Data.Functor.Identity (Identity (..))
+import           Data.Generics.Product
 import           Data.List (intersperse)
+import           Data.Maybe (fromJust)
+import           Data.Ratio
+import           GHC.Generics (Generic)
 import           Graphics.Vty (defAttr)
 import qualified Graphics.Vty as V
+import qualified Streaming.Prelude as S
+import           StreamingMidi
 import           Types
 import           Utils
 
@@ -50,25 +64,57 @@ bar c p = vBox
   ]
 
 
-chordApp :: App () e String
+data ChordAppState = CAS
+  { casBeat   :: Int
+  , casChords :: Cofree Identity (Chord PitchClass)
+  } deriving (Eq, Ord, Show, Generic)
+
+
+data ChordAppEvent = TickBeat
+  deriving (Eq, Ord, Show, Enum, Bounded, Generic)
+
+
+
+getN :: Int -> Cofree Identity a -> [a]
+getN 0 _         = []
+getN n (a :< as) = a : getN (n - 1) (coerce as)
+
+
+coiterCycle :: [a] -> Cofree Identity a
+coiterCycle = unfold (coerce . fromJust . uncons) . cycle
+
+
+stateToBars :: ChordAppState -> [Widget n]
+stateToBars s =
+  zipWith (flip bar)
+  [realToFrac $ casBeat s % 4, 0, 0, 0]
+  $ getN 4 (casChords s)
+
+
+
+
+chordApp :: App ChordAppState ChordAppEvent String
 chordApp = App
   { appDraw
       = pure
-      . pure
       . withBorderStyle unicode
       . joinBorders
       . border
       . hBox
       . intersperse vBorder
-      . replicate 4
-      $ bar (Maj7 A `Over` D) 0.25
+      . stateToBars
   , appChooseCursor
       = const
       $ const Nothing
-  , appHandleEvent
-      = const
-      . const
-      $ halt @_ @String ()
+  , appHandleEvent = \s e ->
+      case e of
+        AppEvent TickBeat ->
+          continue $ s & field @"casBeat" .~ mod (casBeat s + 1) 4
+                       & field @"casChords" %~
+                             if casBeat s == 3
+                                then runIdentity . unwrap
+                                else id
+        _ -> halt s
   , appStartEvent
       = pure
   , appAttrMap
@@ -76,4 +122,27 @@ chordApp = App
       . attrMap V.defAttr
       $ pure (theBaseAttr, bg V.white)
   }
+
+
+main :: IO ()
+main = do
+  eventChan <- newBChan 10
+
+  forkIO $
+    S.mapM_ (const $ writeBChan eventChan TickBeat)
+      . S.filter (<= Quarter)
+      $ clockStream 60
+
+  void
+     . customMain
+         (V.mkVty V.defaultConfig)
+         (Just eventChan)
+         chordApp
+     . CAS 0
+     $ coiterCycle
+       [ Maj C
+       , Min A
+       , Maj F
+       , Maj G
+       ]
 
